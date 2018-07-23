2ee392db99c76cf4cdb8b9c57b8e83168423d85b
satyamkapoor001 <41478340+satyamkapoor001@users.noreply.github.com>
2018-07-24 05:12:05 +0530

    roll a ball

---
+++ Assets/scripts/player_controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour {
+    private Rigidbody rb;
+    public float speed;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float movevertical = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(movehorizontal, 0.0f, movevertical);
+        rb.AddForce(move*speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pick up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
